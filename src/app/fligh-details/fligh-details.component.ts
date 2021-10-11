7f51ed3720ab6ce7422b6636d71a675808ded4e3
JaySharma9644 <jksharma.mail9644@gmail.com>
2021-10-11 19:30:52 +0530

    Initial commit

---
+++ src/app/fligh-details/fligh-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FlightsModel } from '../store/flightsStore/flight.model';
+import { Store, select } from '@ngrx/store';
+import * as FilterActions from '../store/flightsStore/act.action';
+
+@Component({
+  selector: 'app-fligh-details',
+  templateUrl: './fligh-details.component.html',
+  styleUrls: ['./fligh-details.component.css'],
+})
+export class FlighDetailsComponent implements OnInit {
+  FlightsList = [];
+  flights$: Observable<FlightsModel[]>;
+  constructor(private store: Store<FlightsModel>) {
+    // this.flights$ = this.store.pipe(select('flights'));
+    this.store.select('flights').subscribe((res) => {
+      this.FlightsList = res['flights'] ? res['flights'] : [];
+    });
+  }
+
+  ngOnInit() {}
+}
